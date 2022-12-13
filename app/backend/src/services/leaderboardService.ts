20acedb8f045afd5db024f52f1d771dac359f9a0
Maikel <maikelmysilvestre@gmail.com>
2022-12-13 04:55:22 -0300

    fix commit

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -9,16 +9,6 @@ export default class LeaderboardService {
   teamsService = new TeamsService();
   matchesModel = MatchesModel;
 
-  // async getHomeLeaderboard() {
-  //   const allMatches = await this.matchesService.getAll();
-  //   const finishedMatches = allMatches.filter((e) => e.inProgress === false);
-  //   const allTeams = this.teamsService.getAll();
-  //   console.log('allteams:', allTeams);
-  //   console.log('finished:', finishedMatches);
-
-  //   // const tableHomeMatches = loadHomeMatchs(finishedMatches, allTeams);
-  // }
-
   async getHomeLeaderboard() {
     const result = await this.matchesModel.findAll();
     const allMatches = result.map((e) => e.dataValues) as IMatches[];
