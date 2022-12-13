917a78ff6ce3029782f157b12e13255767b27b01
Maikel <maikelmysilvestre@gmail.com>
2022-12-12 21:28:50 -0300

    req 11-12

--- app/backend/src/middlewares/loginMiddleware.ts
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -4,7 +4,9 @@ const RegexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
 export default function validateEmailAndPassword(req: Request, res: Response, next: NextFunction) {
   const user = req.body;
-  if (!user.password) return res.status(400).json({ message: 'All fields must be filled' });
+  if (!user.password || !user.email) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
   const isEmail = RegexEmail.test(user.email);
   if (!isEmail) return res.status(401).json({ message: 'Incorrect email or password' });
   next();
